db6b2d0f53a532110adaf441f676b25342050ffb
金鹏 <307531759@qq.com>
2017-01-15 16:31:22 +0800

    1. 第二分支 新加入两个文件 obj1.h obj1.m

---
+++ text/text/obj1.h
@@ -0,0 +1,15 @@
+//
+//  obj1.h
+//  text
+//
+//  Created by apple on 17/1/15.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface obj1 : UIView
+
+@property (nonatomic, assign) int a;
+
+@end
---
+++ text/text/obj1.m
@@ -0,0 +1,21 @@
+//
+//  obj1.m
+//  text
+//
+//  Created by apple on 17/1/15.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "obj1.h"
+
+@implementation obj1
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
