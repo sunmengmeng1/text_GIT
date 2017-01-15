f7e43384995c697cacf029414cda3bfc3a354faf
金鹏 <307531759@qq.com>
2017-01-15 16:34:39 +0800

    1. 在分支1 添加两个文件 sum.h sum.m

---
+++ text/text/sum.h
@@ -0,0 +1,13 @@
+//
+//  sum.h
+//  text
+//
+//  Created by apple on 17/1/15.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sum : UIView
+
+@end
---
+++ text/text/sum.m
@@ -0,0 +1,21 @@
+//
+//  sum.m
+//  text
+//
+//  Created by apple on 17/1/15.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "sum.h"
+
+@implementation sum
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
