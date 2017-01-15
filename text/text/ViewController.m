aa8e771de95c89dece08677f802f5886f0fb1176
金鹏 <307531759@qq.com>
2017-01-15 16:16:47 +0800

    分支2  修改

--- text/text/ViewController.m
+++ text/text/ViewController.m
@@ -17,8 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
     NSLog(@"NO。1");
     NSLog(@"在第二分支开发");
+    NSLog(@"分支2 第二次修改");
+    
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+
 }
 
 - (void)didReceiveMemoryWarning {
